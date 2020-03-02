03f570c63dcfca907ebc1bdfdeacdcc84c5d4db0
Aluno <menesesmirian12@gmail.com>
2020-03-02 09:33:45 -0300

    aulafim

---
+++ aula01.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>IFerno</title>
+  </head>
+  <body>
+    <style media= "screen">
+    h1{
+      color: #FFFFFF;
+      background: #006600;
+    }
+    table, td, th {
+      border: 1px solid black;
+
+    }
+    </style>
+
+    <div "class"= "noticias">
+    <h1>  IFPR começa a erguer as paredes do refeitório em fevereiro de 2020!!!!</h1>
+    <p> O refeitório que era para esta pronto em agosto de 2019, começou em fevereiro de 2020. </p>
+    <p> Alunos do ultimo ano ainda sonham em comer de graça. </p>
+    <p> O que importa é a esperança do brasileiro que nunca morre né. </p>
+      <img src="https://s2.glbimg.com/fnwNq43DubYjKtBxOpE9cGHrxJk=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/A/o/HDjNseRgyz7K08HYrn6g/construcao-civil-01.jpg"  style="width:500px;height:300px;">
+
+    </div>
+
+    <div "class"= "noticias">
+      <h1> Alomoço almejado pelo alunos no futuro refeitório: </h1>
+      <table>
+  <tr>
+    <th>Comida</th>
+    <th>Bebida</th>
+    <th>Sobremesa</th>
+  </tr>
+  <tr>
+    <td>Strogonoff</td>
+    <td>Suco de laranja</td>
+    <td>mousse de maracujá</td>
+  </tr>
+  <tr>
+    <td>Lasanha</td>
+    <td>Suco de maracujá</td>
+    <td>Sorvete de creme</td>
+  </tr>
+  <tr>
+    <td>Frango ao molho</td>
+    <td>Refrigerante</td>
+    <td>Bolo de pote</td>
+  </tr>
+
+</table>
+<p> Esperamos que eles conquistem esse sonho! </p>
+<p> Estamos todos ansiosos <p>
+
+
+    <div "class"= "noticias">
+    <h1>  IFPR começa a fazer reformas, porém sala entra debaixo d'agua!</h1>
+     <p> A escola com ótima qualidade de ensino foi por água abaixo :(  </p>
+     <p> ESTADO ALERTA: epidemia de  dengue pode ter começado pela poça no local!!! </p>
+     <img src=" https://gazetaweb.globo.com/fotosPortal/portal_gazetaweb_com/noticias/foto_grande/201605291728_0843fff08d.jpg" >
+
+    </div>
+
+
+
+
+  </body>
+</html>
