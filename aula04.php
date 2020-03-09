a0b0ae111420b8cbb23e823b92aeebb4bbd5ecca
Aluno <menesesmirian12@gmail.com>
2020-03-09 09:26:51 -0300

    acabou

---
+++ aula04.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04- CSS e HTML</title>
+    <!-- toda a importação de SS deve ser feita dentro do HEAD -->
+    <!-- CNTRL / -->
+      <link href="https://fonts.googleapis.com/css">
+    <link rel= "stylesheet" href= "aula04.css">
+  </head>
+  <body>
+    <div class= "site">
+
+      <div class="topo">
+         Ajude uma causa!
+      </div>
+
+      <div class="conteudo">
+        COMPRE UMA RIFA
+      </div>
+
+      <div class= "rodape">
+        Desenvolvido por Mirian Legal
+      </div>
+    </div>
+
+  </body>
+</html>
