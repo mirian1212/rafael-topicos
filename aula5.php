a064d4400b9ad8e37c2301bec96d9771b7b00676
mirian1212 <menesesmirian12@gmail.com>
2020-03-16 09:53:45 -0300

    aula55

---
+++ aula5.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04- CSS e HTML</title>
+    <!-- toda a importação de SS deve ser feita dentro do HEAD -->
+    <!-- CNTRL / -->
+      <link href="https://fonts.googleapis.com/css">
+    <link rel= "stylesheet" href= "aula5.css">
+  </head>
+  <body>
+    <div class= "site">
+
+      <div class="topo">
+         <h3>  IFPR começa a erguer as paredes do refeitório em fevereiro de 2020!!!!</h3>
+
+         <div class= "menu">
+             <ul>
+               <li><a href="#">Página inicial</a></li>
+               <li><a href="#">Cadastro</a></li>
+               <li><a href="#">Contato</a></li>
+               <li><a href="#">Ajuda</a></li>
+               <li><a href="#">Login</a></li>
+            </ul>
+         </div>
+      </div>
+
+
+
+      <div class="conteudo">
+      <!-- corpo do site -->
+      <div class= "imagem">
+          <img src="https://s2.glbimg.com/fnwNq43DubYjKtBxOpE9cGHrxJk=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/A/o/HDjNseRgyz7K08HYrn6g/construcao-civil-01.jpg"  style="width:400px;height:0px auto;">
+        <h3> O refeitório que era para estar pronto em agosto de 2019, começou em fevereiro de 2020!!! Mas alunos do ultimo ano ainda sonham em comer de graça. </h3>
+     </div>
+        <a href="#" class="botao botao-rosa">Ver mais</a>
+        <a href="#" class="botao botao-verde"> Voltar ao topo</a>
+        <a href="#" class="botao botao-roxo"> Sair</a>
+
+      </div>
+
+      <div class= "rodape">
+        Desenvolvido por Mirian Legal
+      </div>
+    </div>
+
+  </body>
+</html>
